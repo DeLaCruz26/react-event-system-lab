225b2471e88c1c02deeda7c5f3a7e6f9e4e45621
DeLaCruz26 <dametrik.fick@gmail.com>
2021-02-24 21:54:15 -0500

    commit tree 045a59c82d4cacd03a50cf17181dbc7bf16c226c

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+ 
+function EyesOnMe(){
+    
+    const handleFocus = (e) => {
+        console.log('Good!');
+    }
+
+    const handleBlur = (e) => {
+        console.log('Hey! Eyes on me!');
+    }
+
+    return(
+        <button onFocus={handleFocus} onBlur={handleBlur} ></button>
+    )
+}
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
 // Code Keypad Component Here
+import React from 'react'
+
+function Keypad(){
+
+    const handleKeyUp = (e) => {
+        console.log('Entering password...');
+    }
+
+    return(
+    <input onKeyUp={handleKeyUp} type="password"/>
+    )
+}
+export default Keypad
